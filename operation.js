6719d7a874ac5141ddac735453e425865c720fb0
Prakhar <prakharshrivastava109@gmail.com>
2025-12-13 12:41:40 +0530

    using operations in js

---
+++ operation.js
@@ -0,0 +1,15 @@
+//*****************************operations*********** */
+
+let value=3;
+let negative=-value;
+// console.log(negative);
+// console.log(2*2);
+// console.log(4%3);
+// console.log(4+5);
+// console.log(6-9);
+// console.log(2**3);
+
+console.log(1+"2"+2);
+console.log(1+2+"2");
+console.log(1+2+"2"+2);
+
