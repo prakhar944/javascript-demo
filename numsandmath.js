ccabcdb9dbfff9be82bde561e16966b831dfb034
Prakhar <prakharshrivastava109@gmail.com>
2025-12-13 19:19:46 +0530

    adding strings and mathematical functions

---
+++ numsandmath.js
@@ -0,0 +1,25 @@
+//***************************  NUMBER ************************// 
+
+
+const balance=2000;
+// console.log(balance);
+
+const score = new Number(700);
+// console.log(score);
+
+// console.log(score.toString().length );
+// console.log(score.toFixed(3));
+
+const otherString=123.82345;
+// console.log(otherString.toPrecision(3));
+
+const newNumber=1000000;
+// console.log(newNumber.toLocaleString('en-In'));
+
+//****************************************MATHS*************************************//
+console.log(Math);
+// WE CAN USE{ Math.abs,Math.round,Math.ceil,Math.floor,max and min also}
+
+console.log(Math.random()*100);
+const max=6;min=1;
+console.log(Math.floor((Math.random()*(max-min+1))+min));
---
+++ string.js
@@ -0,0 +1,6 @@
+const name="Prakhar";
+const game= 30;
+// console.log(game +name +" Played");
+const gameName= new String('Valorant');
+console.log(`My name is ${name} & I have played ${game} games\n My favourite game is ${gameName}`);
+
