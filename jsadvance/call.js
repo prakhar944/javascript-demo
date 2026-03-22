347c2ed37caf3f215c86d51fe948fb68b6fd93ce
Prakhar <prakharshrivastava109@gmail.com>
2026-03-22 13:32:49 +0530

    call in JS

---
+++ jsadvance/call.js
@@ -0,0 +1,12 @@
+function setUsername(username){
+    this.username=username;
+   console.log("Called");
+   
+}
+function createUser(username,email,password){
+    setUsername.call(this,username);
+    this.email=email;
+    this.password=password;
+}
+const prakhar= new createUser("Prakhar","Prakhar@meta.com","1234")
+console.log(prakhar);
